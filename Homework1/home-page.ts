9e33557e685528a4fd0a721e096f1d17b4aed7ae
Roxanica123 <irimia.andreea.roxana@gmail.com>
2021-02-19 20:31:44 +0200

    Add Twitter request

--- Homework1/home-page.ts
+++ Homework1/home-page.ts
@@ -1,17 +1,25 @@
-import * as  http from "http";
 import { Ok } from "./action_results";
 import { MyRequest } from "./request";
+import { twitter_bearer_token } from "./secrets";
 
 
 export class HomePage {
     public static readonly instance: HomePage = new HomePage()
-    private static readonly issLocationUrl = "http://api.open-notify.org/iss-now.json"
     constructor() {
     }
     async get_response() {
-        const options = { host: "api.open-notify.org", path: "/iss-now.json", method: 'GET' }
-        const response = await new MyRequest("GET", "api.open-notify.org","/iss-now.json").send();
-        console.log(response)
+        const response_iss = await new MyRequest("GET", "api.open-notify.org", "/iss-now.json").send();
+        console.log(response_iss)
+        HomePage.get_tweet();
         return new Ok("?")
+    }
+    private static async get_tweet() {
+        const tweetQuery: string = "?ids=1362823946148732931&tweet.fields=geo&expansions=geo.place_id&place.fields=geo,contained_within";
+        const header = {
+            "Authorization": "Bearer " + twitter_bearer_token
+        };
+        const path = "/2/tweets" + tweetQuery;
+        const response_twitter = await new MyRequest("GET", "api.twitter.com", path, header, "https").send();
+        console.log(response_twitter);
     }
 }
---
+++ Homework1/secrets.ts
@@ -0,0 +1,1 @@
+export const twitter_bearer_token: string = "AAAAAAAAAAAAAAAAAAAAAJSmKQEAAAAAWtqYdL05KM8NBGaC5fguHwKa50o%3DV1qyDUpMCMyoIMpdGdk1vHqDJt2DrDvTJXeE5DStOc0ifZitgW"  
