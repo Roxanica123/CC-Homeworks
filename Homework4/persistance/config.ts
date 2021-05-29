c70dd6e503c8ca0918f53a9da519d1eaa1bce42e
Roxanica123 <irimia.andreea.roxana@gmail.com>
2021-05-29 12:58:57 +0300

    Use certificates for signing and verifying access tokens

--- Homework4/persistance/config.ts
+++ Homework4/persistance/config.ts
@@ -1,5 +1,3 @@
-  import dotenv from "dotenv";
-dotenv.config();
 
 export interface CosmosConfig {
   endpoint: string,
--- Homework4/services/index.ts
+++ Homework4/services/index.ts
@@ -1,2 +1,3 @@
 export * from "./auth_service"
 export * from "./jwt_service"
+export * from "./pfx_service"
